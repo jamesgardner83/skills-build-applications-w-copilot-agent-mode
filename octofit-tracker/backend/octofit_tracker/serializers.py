a6b230f3a8133f31bf8dd0dfb5879031f8904718
James Gardner <79858048+jamesgardner83@users.noreply.github.com>
2026-01-20 21:33:43 +0000

    feat: enhance serializers to include nested user and team details

--- octofit-tracker/backend/octofit_tracker/serializers.py
+++ octofit-tracker/backend/octofit_tracker/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
 from .models import User, Team, Activity, Workout, Leaderboard
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
 class TeamSerializer(serializers.ModelSerializer):
+    members = UserSerializer(many=True, read_only=True)
     class Meta:
         model = Team
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'members']
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,16 +18,24 @@ class UserSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ActivitySerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Activity
-        fields = '__all__'
+        fields = ['id', 'user', 'activity_type', 'duration', 'date']
+
+class TeamNameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = ['id', 'name']
 
 class WorkoutSerializer(serializers.ModelSerializer):
+    suggested_for = TeamNameSerializer(many=True, read_only=True)
     class Meta:
         model = Workout
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'suggested_for']
 
 class LeaderboardSerializer(serializers.ModelSerializer):
+    team = TeamNameSerializer(read_only=True)
     class Meta:
         model = Leaderboard
-        fields = '__all__'
+        fields = ['id', 'team', 'total_points']
